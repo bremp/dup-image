d7b2c0bf4b060966cd98910a1dabf6983eff0384
bremp <byadao@gmail.com>
2021-07-22 15:07:52 -1000

    Added initial code.

--- src/index.js
+++ src/index.js
@@ -1,44 +1,59 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+// local dependencies
+const io = require("./main/io");
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+if (require("electron-squirrel-startup")) {
+  // eslint-disable-line global-require
   app.quit();
 }
 
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
+// open a window
+const openWindow = () => {
+  const win = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 500,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      // enableRemoteModule: true,
+    },
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  win.loadFile(path.resolve(__dirname, "render/html/index.html"));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  win.webContents.openDevTools();
+  return win; // return window
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", () => {
+  const win = openWindow();
+
+  // watch files
+  // io.watchFiles(win);
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
+    openWindow();
   }
 });
 
---
+++ src/render/js/renderer.js
@@ -0,0 +1,47 @@
+const dragDrop = require("drag-drop");
+const { ipcRenderer } = require("electron");
+
+// local dependencies
+const dom = require("../js/dom");
+
+/*****************************/
+
+// get list of files from the `main` process
+ipcRenderer.invoke("app:get-files").then((files = []) => {
+  //   dom.displayFiles(files);
+  console.log("get list of files from the `main` process");
+});
+
+// handle file delete event
+ipcRenderer.on("app:delete-file", (event, filename) => {
+  document.getElementById(filename).remove();
+});
+
+/*****************************/
+
+// add files drop listener
+dragDrop("#uploader", (files) => {
+  const _files = files.map((file) => {
+    return {
+      name: file.name,
+      path: file.path,
+    };
+  });
+
+  // send file(s) add event to the `main` process
+  ipcRenderer.invoke("app:on-file-add", _files).then(() => {
+    ipcRenderer.invoke("app:get-files").then((files = []) => {
+      dom.displayFiles(files);
+    });
+  });
+});
+
+// open filesystem dialog
+window.openDialog = () => {
+  console.log("Open dialog clicked!");
+  //   ipcRenderer.invoke("app:on-fs-dialog-open").then(() => {
+  //     ipcRenderer.invoke("app:get-files").then((files = []) => {
+  //       dom.displayFiles(files);
+  //     });
+  //   });
+};
